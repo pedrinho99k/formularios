25b8e3590162768875bae35abef6602940739bee
Pedro <151837159+hospitalhr@users.noreply.github.com>
2024-06-12 09:00:28 -0300

    removido o filtro  do buscar-registros-formulario-ativos-json

--- php/Funcoes/buscar-registros-formulario-ativos-json.php
+++ php/Funcoes/buscar-registros-formulario-ativos-json.php
@@ -1,8 +1,29 @@
 <?php
 $cod_form = $_POST['cod_formulario'];
 
-if (empty($_POST['cod_usuario'])) {
-    $sql = "SELECT *
+// if (empty($_POST['cod_usuario'])) {
+//     $sql = "SELECT *
+// FROM fm_registros AS reg 
+// JOIN fm_formularios as form ON reg.reg_codigo_formulario = form.form_codigo
+// JOIN fm_usuarios as usu ON reg.reg_codigo_usuario = usu.usu_codigo
+// WHERE form.form_codigo = $cod_form
+// AND form.form_ativo = 'SIM'
+// AND reg.reg_ativo = 'SIM' 
+// ORDER BY reg.reg_codigo DESC";
+// } else {
+//     $cod_usuario = $_POST['cod_usuario'];
+//     $sql = "SELECT *
+// FROM fm_registros AS reg 
+// JOIN fm_formularios as form ON reg.reg_codigo_formulario = form.form_codigo
+// JOIN fm_usuarios as usu ON reg.reg_codigo_usuario = usu.usu_codigo
+// WHERE reg.reg_codigo_usuario = $cod_usuario 
+// AND form.form_codigo = $cod_form
+// AND form.form_ativo = 'SIM'
+// AND reg.reg_ativo = 'SIM' 
+// ORDER BY reg.reg_codigo DESC";
+// }
+
+$sql = "SELECT *
 FROM fm_registros AS reg 
 JOIN fm_formularios as form ON reg.reg_codigo_formulario = form.form_codigo
 JOIN fm_usuarios as usu ON reg.reg_codigo_usuario = usu.usu_codigo
@@ -10,18 +31,7 @@ WHERE form.form_codigo = $cod_form
 AND form.form_ativo = 'SIM'
 AND reg.reg_ativo = 'SIM' 
 ORDER BY reg.reg_codigo DESC";
-} else {
-    $cod_usuario = $_POST['cod_usuario'];
-    $sql = "SELECT *
-FROM fm_registros AS reg 
-JOIN fm_formularios as form ON reg.reg_codigo_formulario = form.form_codigo
-JOIN fm_usuarios as usu ON reg.reg_codigo_usuario = usu.usu_codigo
-WHERE reg.reg_codigo_usuario = $cod_usuario 
-AND form.form_codigo = $cod_form
-AND form.form_ativo = 'SIM'
-AND reg.reg_ativo = 'SIM' 
-ORDER BY reg.reg_codigo DESC";
-}
+
 include_once("../conexao/conexao.php");
 $conecta = Conectar();
 $stm = $conecta->prepare($sql);
